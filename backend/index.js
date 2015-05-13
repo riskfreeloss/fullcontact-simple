2b842ff01ff841c84b2537b379dd67d01f1ab51f
Kuvakin Sergey <kuvakin@gmail.com>
2015-05-13 17:36:41 +0300

    refactoring

--- backend/index.js
+++ backend/index.js
@@ -1,126 +1,102 @@
-// fullcontact-simple
-var express = require('express'),
-	bodyParser = require('body-parser'),
-	FullContact = require('fullcontact'),
-    inno = require('./innometrics-helper'),
-    app = express();
+var express = require('express'), // Allows easily build a web server
+    bodyParser = require('body-parser'),
+    InnoHelper = require('innometrics-helper'); // Innometrics helper to work with profile cloud
 
-app.set('port', (process.env.PORT || 5000));
+var FullContact = require('fullcontact'),
+    fullcontact = null;
 
-inno.setVars({
-    bucketName: process.env.INNO_BUCKET_ID /* || 'first-bucket' */,
-    appKey: process.env.INNO_APP_KEY /* || 'ZYSBCxyg5MP4P293'*/,
-    appName: process.env.INNO_APP_ID /* || 'full-contact-simple'*/,
-    groupId: process.env.INNO_COMPANY_ID /* || 9 */,
-    apiUrl: process.env.INNO_API_HOST /* || 'http://prerelease.innomdc.com/v1' */,
-    auth: {
-        user: '4.superuser',
-        pass: 'test'
-    },
-    // only for updateProfile
-    profileId: '12345',
-    collectApp: 'rest-api',
-    section: 'first-section'
+var app = express(),
+    port = parseInt(process.env.PORT, 10);
+
+// Parse application/json request
+app.use(bodyParser.json());
+
+/**
+ * If your app's frontend part is going to communicate directly with backend, you need to allow this
+ * https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+ */
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
-var fullcontact;
+/**
+ * Init params from environment variables. Innometrics platform sets environment variables during install to Paas.
+ * In case of manual install of backend part, you need to setup these manually.
+ */
+var config = {
+    groupId: process.env.INNO_COMPANY_ID,
+    bucketName: process.env.INNO_BUCKET_ID,
+    appName: process.env.INNO_APP_ID,
+    appKey: process.env.INNO_APP_KEY,
+    apiUrl: process.env.INNO_API_HOST
+};
+var inno = new InnoHelper(config);
 
-console.log("Setting up FullContact...");
-inno.getSettings({
-        	vars: inno.getVars()
-	    }, function (err, settings) {
-	        if (err) {
-  				console.log("Error: " + err.message + " vars: "+JSON.stringify(inno.getVars()));
-	        }
+var errors = [];
+var tasks = [];
 
-	    	var fcApiKey = settings ? settings.fcApiKey : process.env.FULLCONTACT_API_KEY;
-
-			fullcontact = new FullContact(fcApiKey);
-		});
-
-
-app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.json())
-
-app.get('/', handleRequests);
-app.post('/', handleRequests);
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
-function handleRequests(request, response) {
-
-
-	console.log("Received http request, method: "+JSON.stringify(request.method)+" params: "+JSON.stringify(request.params)+" query: "+JSON.stringify(request.query)+" body: "+JSON.stringify(request.body));
-
-	inno.getSettings({
-		vars: inno.getVars()
-	}, function (err, settings) {
-		if (err) {
-			return response.json({
-				error: err.message,
-				vars: inno.getVars()
-			});
-		}
-
-		var emailParam = request.param("email") || settings.email;
-		if(request.body && request.body.profile)
-		{
-			var sessions = request.body.profile.sessions || [];
-			for(var i = 0; i < sessions.length; i++)
-			{
-				var events = request.body.profile.sessions[i].events || [];
-				for(var j = 0; j < events.length; j++)
-				{
-					var ev = events[j];
-					if(ev.definitionId && ev.definitionId == "fake-login-event" && ev.data && ev.data.email)
-					{
-						emailParam = ev.data.email;
-					}
-				}
-			}
-		}
-		else
-		{
-			console.log("Unrecognized input in body, using profile ID "+inno.getVars().profileId+" and email: "+emailParam);
-		}
-
-		fullcontact.person.email(emailParam, function (err, data) {
-
-			console.log("Attempting to retrieve social media data for "+emailParam);
-
-			var innoVars = inno.getVars();
-			if(request.body && request.body.profile && request.body.profile.id) {
-				console.log("parsed profileId to update: "+request.body.profile.id);
-				innoVars.profileId = request.body.profile.id;
-			}
-			else
-			{
-				console.log("no profile id in body, using "+innoVars.profileId);
-				console.log(request.body);
-			}
-
-			inno.updateProfile({
-				vars: innoVars,
-				data: {
-						'socialMediaData': data.socialProfiles ? JSON.stringify(data.socialProfiles) : ""
-					}
-				}, function (err) {
-					var result = 'Attempted to retrieve data for <pre>'+emailParam+'</pre><br>';
-					if(err) {
-						result = result+'ERROR:<br>'+err+'</br>';	
-						console.log('ERROR: '+err);
-					}
-					if(data) {
-						result = result+JSON.stringify(data)+'</br>';	
-						//console.log('data returned!'+JSON.stringify(data));
-						console.log('data returned: '+JSON.stringify(data.socialProfiles));
-					}
-					//result = result + '<br><pre>'+JSON.stringify(process.env)+'</pre></br>';
-					
-					return response.send(result);
-				});
-			});
-	});
+function jsonError(res, error) {
+    errors.push(error);
+    return res.json({
+        error: null
+    });
 }
+
+// POST request to "/" is always expected to recieve stream with events
+app.post('/', function (req, res) {
+    var indexTask = tasks.push('---');
+    inno.getAppSettings(function (error, settings) {
+        if (error) {
+            return jsonError(res, error);
+        }
+        fullcontact = fullcontact || new FullContact(settings.fcApiKey);
+        inno.getProfile(req.body, function (error, parsedData) {
+            if (error) {
+                return jsonError(res, error);
+            }
+            var emailParam = parsedData.data && parsedData.data.email || settings.email;
+            var profileId = parsedData.profile.id;
+            var section = parsedData.session.section;
+            var collectApp = parsedData.session.collectApp;
+            tasks[indexTask - 1] = emailParam;
+
+            fullcontact.person.email(emailParam, function (error, data) {
+                if (error) {
+                    return jsonError(res, error);
+                }
+
+                inno.setProfileAttributes({
+                    collectApp: collectApp,
+                    section: section,
+                    profileId: profileId,
+                    attributes: {
+                        socialMediaData: data && data.socialProfiles && JSON.stringify(data.socialProfiles)
+                    }
+                }, function (error) {
+                    if (error) {
+                        return jsonError(res, error);
+                    }
+                    res.json({
+                        error: null
+                    });
+                });
+            });
+        });
+
+    });
+});
+
+app.get('/', function (req, res) {
+    errors = errors.slice(-10);
+    tasks = tasks.slice(-10);
+    res.json({
+        error: errors,
+        tasks: tasks
+    });
+});
+
+// Starting server
+var server = app.listen(port, function () {
+    console.log('Listening on port %d', server.address().port);
+});
